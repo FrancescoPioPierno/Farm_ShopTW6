704842075cf01fffb552377859d9d15545939cf2
FrancescoPioPierno <62958435+FrancescoPioPierno@users.noreply.github.com>
2021-06-01 12:17:47 +0200

    Add files via upload

---
+++ shop/carts/carts.py
@@ -0,0 +1,39 @@
+from flask import redirect, render_template, url_for, flash, request, session, current_app
+from shop import db, app
+from shop.products.models import Addproduct
+#spiegare questo package simplejson
+import simplejson as json
+
+def MagerDicts(dict1, dict2):
+    if isinstance(dict1, list) and isinstance(dict2, list):
+        return dict1+dict2
+    elif isinstance(dict1, dict) and isinstance(dict2, dict):
+        return dict(list(dict1.items()) + list(dict2.items()))
+    return False
+
+
+
+@app.route('/addcart', methods=['POST'])
+def AddCart():
+    try:
+        product_id = request.form.get('product_id')
+        quantity = request.form.get('quantity')
+        product = Addproduct.query.filter_by(id=product_id).first()
+        if product_id and quantity and request.method == 'POST':
+            DictItems = {product_id:{'name':product.name, 'price':product.price, 'discount':product.discount,
+                                     'quantiy':quantity, 'image':product.image_1}}
+            if 'Shoppingcart' in session:
+                print(session['Shoppingcart'])
+                if product_id in session['Shoppingcart']:
+                    print("Questo prodotto è già nel tuo carrello")
+                else:
+                    session['Shoppingcart'] = MagerDicts(session['Shoppingcart'], DictItems)
+                    return redirect(request.referrer)
+            else:
+                session['Shoppingcart'] = DictItems
+                return redirect(request.referrer)
+
+    except Exception as e:
+        print(e)
+    finally:
+        return redirect(request.referrer)
