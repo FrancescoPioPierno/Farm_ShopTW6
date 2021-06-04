296727d10b6c5729f426c5c7213291f3086ae70b
FrancescoPioPierno <62958435+FrancescoPioPierno@users.noreply.github.com>
2021-06-04 11:44:22 +0200

    Add files via upload

--- shop/__init__.py
+++ shop/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_uploads import IMAGES, UploadSet, configure_uploads, patch_request_class
 import os
+from flask_login import LoginManager
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -12,6 +13,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static/images')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='customerLogin'
+login_manager.needs_refresh_message_category='danger'
+login_manager.login_message=u"Per favore effettua prima il login"
+
+
 
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
@@ -24,4 +33,5 @@ patch_request_class(app)
 from shop.admin import routes
 from shop.products import routes
 from shop.carts import carts
+from shop.customers import routes
 
